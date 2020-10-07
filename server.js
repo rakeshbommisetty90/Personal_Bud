600c73f9319efd5e2ff4d76c32edbd7263c2b21a
Rakesh Bommisetty <rakeshbommisetty90@gmail.com>
2020-10-07 18:21:59 -0400

    Added CORS Changes

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
+// Budget API
 const express=require('express');
+const cors=require('cors');
 const app=express();
 const port =3000;
 const budget=require('./budget.json');
-app.use('/',express.static('public'));
-
-app.get('/h',(req,res)=>{
-    res.send("Hello World");
-});
+app.use(cors());
 
 app.get('/budget', (req,res)=>{
     res.send(budget);
 })
 app.listen(port,()=>{
-    console.log(`This application is listening at this http://localhost:${port}`)
+    console.log(` API Served in this http://localhost:${port}`)
 });
